3f46c9c5f6429974c280662d1651d9f35230ece4
Alessio Dal Santo <alessiodalsanto@live.it>
2018-09-05 19:52:50 +0200

    Aggiunto sensore magnetico per accensione e spegnimento del led RGB

--- led_rgb_fotosensibile.ino
+++ led_rgb_fotosensibile.ino
@@ -2,6 +2,7 @@
 const int LED_B_Pin = 5;
 const int LED_G_Pin = 6;
 const int LED_R_Pin = 10;
+const int HALL_Pin = 11;
 const int res = A0;
 
 int value = 0;
@@ -16,19 +17,27 @@ void setup() {
 	pinMode(LED_G_Pin, OUTPUT);
 	pinMode(LED_R_Pin, OUTPUT);
 	pinMode(LED_B_Pin, OUTPUT);
+	pinMode(HALL_Pin, INPUT);
 }
 
 void loop() {
 
-	//lettura sensore A0 ovvero la tensione della fotoresistenza
-	value = analogRead(res);
-	value_R = value / 4;
+	int hall_state = digitalRead(HALL_Pin);
 
-	Serial.println(value_R);
-	//il 255 serve a invertire il funzionamento più c'è luce meno si accende il led
-	//Il led rosso serve a rappresentare i colori caldi mentre il blu i freddi
-	analogWrite(LED_R_Pin, 255 - value_R);
-	analogWrite(LED_B_Pin, value_R);
-	delay(200);
+	if (hall_state == HIGH) {
+		//lettura sensore A0 ovvero la tensione della fotoresistenza
+		value = analogRead(res);
+		value_R = value / 4;
+
+		Serial.println(value_R);
+		//il 255 serve a invertire il funzionamento più c'è luce meno si accende il led
+		//Il led rosso serve a rappresentare i colori caldi mentre il blu i freddi
+		analogWrite(LED_R_Pin, 255 - value_R);
+		analogWrite(LED_B_Pin, value_R);
+		delay(200);
+	}else{
+		analogWrite(LED_R_Pin, 0);
+		analogWrite(LED_B_Pin, 0);
+	}
 }
 
