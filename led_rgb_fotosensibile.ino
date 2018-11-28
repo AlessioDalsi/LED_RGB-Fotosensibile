f351162095186cecc91e65df39b999185cc3b4d1
Alessio Dal Santo <alessiodalsanto@live.it>
2018-11-28 18:58:41 +0100

    Ottimizzazione progetto: prima parte funzionamento led con magnete

--- led_rgb_fotosensibile.ino
+++ led_rgb_fotosensibile.ino
@@ -1,4 +1,6 @@
 // costanti per individuare i LED R, G e B rispettivamente ai pin 5, 6, 10 (a quanto pare il pin 7 del mio arduino non funziona)
+#include <ESP8266.h>
+
 const int LED_B_Pin = 5;
 const int LED_G_Pin = 6;
 const int LED_R_Pin = 10;
@@ -24,20 +26,30 @@ void loop() {
 
 	int hall_state = digitalRead(HALL_Pin);
 
-	if (hall_state == HIGH) {
-		//lettura sensore A0 ovvero la tensione della fotoresistenza
-		value = analogRead(res);
-		value_R = value / 4;
+	//if (hall_state == HIGH) {
+	//	//lettura sensore A0 ovvero la tensione della fotoresistenza
+	//	value = analogRead(res);
+	//	value_R = value / 4;
+	//		
 
-		Serial.println(value_R);
-		//il 255 serve a invertire il funzionamento più c'è luce meno si accende il led
-		//Il led rosso serve a rappresentare i colori caldi mentre il blu i freddi
-		analogWrite(LED_R_Pin, 255 - value_R);
-		analogWrite(LED_B_Pin, value_R);
-		delay(200);
+	//	Serial.println(value_R);
+	//	//il 255 serve a invertire il funzionamento più c'è luce meno si accende il led
+	//	//Il led rosso serve a rappresentare i colori caldi mentre il blu i freddi
+	//	analogWrite(LED_G_Pin, 255 - value_R);
+	//	analogWrite(LED_B_Pin, value_R);
+	//	delay(200);
+	//}else{
+	//	analogWrite(LED_R_Pin, 0);
+	//	analogWrite(LED_B_Pin, 0);
+	//}
+	if(hall_state == HIGH){
+		analogWrite(LED_R_Pin, 255);
+		analogWrite(LED_B_Pin, 0);
+		analogWrite(LED_G_Pin, 0);
 	}else{
 		analogWrite(LED_R_Pin, 0);
-		analogWrite(LED_B_Pin, 0);
+		analogWrite(LED_B_Pin, 255);
+		analogWrite(LED_G_Pin, 0);
 	}
 }
 
